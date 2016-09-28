6a959d5eb90da6deea0adcd259b1285091c313ae
William Hughes <williamh@catalyst.net.nz>
2016-09-29 12:21:48 +1300

    Remove unneeded files

--- debian/ibeacon.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ibeacon package
-#
-0 4	* * *	root	[ -x /usr/bin/ibeacon_maintenance ] && /usr/bin/ibeacon_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ibeacon):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ibeacon" command="/usr/bin/ibeacon"
